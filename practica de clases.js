a27433a82c8fce0403eab16755172225837d118a
slenis <slenis@bancolombia.com.co>
2021-11-25 12:31:12 -0500

    se adjunta examen

---
+++ practica de clases.js
@@ -0,0 +1,29 @@
+/*class Animal {
+    nombre;
+    edad;
+    especie;
+    sonido;
+
+    constructor(nombre,edad,especie,sonido){
+        
+        this.nombre = nombre;
+        this.edad = edad;
+        this.especie = especie;
+        this.sonido = sonido;
+        console.log("ha nacido un animal y se llama: " + this.nombre )
+    }
+ruido(){
+if(this.nombre = "pato"){
+    console.log("soy un Pato gonorrea");
+}
+
+}
+    
+}
+
+const desconocido = new animal("Pato",36,"Pato","cuak");
+console.log(desconocido.sonido);
+
+console.log(desconocido.ruido())
+const otro = new animal("perro",7,"canino","guau guau")*/
+
